0fb8f52be94fc5f782ede07514a8fdebea3d6bfa
Jack Su <suj052003@gmail.com>
2020-12-03 10:05:10 -0800

    implemented Fib number

--- .ipynb_checkpoints/GitPracticeProject.py
+++ .ipynb_checkpoints/GitPracticeProject.py
@@ -3,8 +3,12 @@
 # Practice Project to demonstrate Git workflow
 
 def main():
-    pass
+    # find the nth Fibonacci number
+    # e.g. 1,1,2,3,5,8,13,21,34, ...
+    n = int(input("what Fibonacci number do you wabt to find"))
 
+    if n in [1, 2]:
+        print(f"The{n}st/nd Fib number is 1.")
 
 if __name__ == "__main__":
     main()
