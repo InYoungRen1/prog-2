c2f8a46a16a566f27ed28d43f53413369dce6589
Jack Su <suj052003@gmail.com>
2020-12-07 11:36:14 -0800

    quiz 5 questions updated

--- quiz.py
+++ quiz.py
@@ -1,1 +1,11 @@
 # quiz
+
+print("Hello I'm William and I want to play a game with you!")
+question_one = float(input("Please answer question: 25*4"))
+if input == 100:
+    print("You are correct")
+else:
+    print("X")
+
+question_two = input("Please answer question 2: What is my name?").lower()
+if input == william
